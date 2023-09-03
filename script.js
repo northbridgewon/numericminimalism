3a70ed12c250b56cb0a6da8da45fd8dbc6369049
æx <6d6b2f61796363@gmail.com>
2023-09-03 09:49:59 -0500

    chai

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById('staticBackground');
+var canvas = document.getElementById('main');
 var context = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
@@ -7,7 +7,7 @@ canvas.height = window.innerHeight;
 window.onresize = function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    var haikuDiv = document.getElementById("haiku");
+    var haikuDiv = document.getElementById("data");
     haikuDiv.style.top = window.innerHeight / 2 + "px";
     haikuDiv.style.left = window.innerWidth / 2 + "px";
 };
